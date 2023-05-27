9b267e6f3c14f6a535107858a55ad314f007b0af
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-05-27 13:03:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,7 +38,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, message.from_user.username, temp.U_NAME))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
+            InlineKeyboardButton("➕️ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕️", callback_data="help")
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
