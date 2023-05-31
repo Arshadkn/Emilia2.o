d75da9ccdae5a820c421948fa324e8c6303d57c2
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-05-31 16:36:12 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -347,12 +347,12 @@ If you still do not get it. Movie Name & year after @admin. Example: Add @admin 
 
 If you ask for a movie released in theaters, you will not get it, only if it is released on ott Dvd
 """
-    SPELL_CHECK_MAL = """
+    SPELL_CHECK_MAL = """<l>
 Google, yendex എന്ന് എഴുതിയിരിക്കുന്ന ഏതെങ്കിലും ബട്ടണിൽ ക്ലിക്ക് ചെയ്ത് ശരിയായ സിനിമയുടെ പേര് കണ്ടെത്തി ഇവിടെ നൽകുക എന്നാലേ സിനിമ / Tv . Web സീരിയസ് കിട്ടുകയുള്ളു.. 
 
 എന്നിട്ടും കിട്ടുന്നില്ല എങ്കിൽ. @admin ശേഷം മൂവി Name & year. Example : @admin kala 2020 ഈ രീതിയിൽ  ഗ്രൂപ്പിൽ സെന്റ് ചെയുക. 24 മണിക്കൂറിനുള്ളിൽ അഡ്മിൻ അപ്‌ലോഡ് ചെയ്യും
 
-തിയേറ്ററിൽ റിലീസ് ആയ മൂവിയാണ് ചോദിച്ചതെങ്കിൽ കിട്ടില്ല ott Dvd റിലീസ് ആയാൽ മാത്രമേ കിട്ടുള്ളൂ
+തിയേറ്ററിൽ റിലീസ് ആയ മൂവിയാണ് ചോദിച്ചതെങ്കിൽ കിട്ടില്ല ott Dvd റിലീസ് ആയാൽ മാത്രമേ കിട്ടുള്ളൂ</i>
 """
     
 
