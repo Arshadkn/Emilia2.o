659b2ca4a430e30e287027ddbd914d153cb7900a
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-05-31 16:54:55 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -20,9 +20,17 @@ disabled_group=filters.create(disabled_chat)
 
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
+    buttons = [[
+        InlineKeyboardButton('📵 Aᴅᴍɪɴ', url=f'https://t.me/adpsycho'), 
+        InlineKeyboardButton(f'⚠️ Bᴀɴ Rᴇᴀsᴏɴ', 'banreo')
+    ]]
+    reply_markup=InlineKeyboardMarkup(buttons)
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
-
+    m=await message.reply_sticker("CAACAgUAAxkBAAEILFpkFHQXxRb5tWqOlEtyUIqu9qs_KAACJAAD046EI5b3vYsJLhc2LwQ")
+    await message.reply(
+          text=f'<code>Sorry Dude, You are Banned to use Me.My Admin Warned You to use my now You are Break My Rules That is Ban Reason Ask My Admin To Unban You\n\nക്ഷമിക്കണം സുഹൃത്തേ, നിങ്ങൾ എന്നെ ഉപയോഗിക്കുന്നതിന് വിലക്കപ്പെട്ടിരിക്കുന്നു. എന്റെ അഡ്മിൻ നിങ്ങൾക്ക് മുന്നറിയിപ്പ് നൽകി, ഇപ്പോൾ എന്റെ നിയമങ്ങൾ നിങ്ങൾ ലംഘിക്കുകയാണ്, അതാണ് വിലക്ക് കാരണം നിങ്ങളെ അൺബാൻ ചെയ്യാൻ എന്റെ അഡ്മിനോട് ആവശ്യപ്പെടുക</code>', 
+          reply_markup=reply_markup)
+    
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
