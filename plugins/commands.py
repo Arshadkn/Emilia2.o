025a430cb5ba1324c6e136e8718154705e8887dd
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-04 23:16:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -519,7 +519,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
             ]]
-        dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
+        dp = await message.reply_text(f"✅ Report Send Successful ✅\n\n👤 Rᴇᴘᴏʀᴛᴇᴅ ᴜsᴇʀ : {message.from_user.mention}ji"), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
         await asyncio.sleep(10) 
         await dp.delete()
         await message.delete()
