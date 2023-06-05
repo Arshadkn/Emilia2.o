508de7423e90928c8f2412b9f8ce0434c0d7ff8b
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-05 21:15:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -520,6 +520,7 @@ async def start(client, message):
     date = curr.strftime('%d-%B-%Y')
     time = curr.strftime('%I:%M:%S %p')
     day = curr.strftime("%A")
+    ido = message.from_user.id
     search = message.text
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[           
@@ -527,23 +528,7 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
             ]]
-        await query.message.edit_text(
-            text="▣▢▢▢▢"
-        )
-        await query.message.edit_text(
-            text="▣▣▢▢▢"
-        )
-        await query.message.edit_text(
-            text="▣▣▣▢▢"
-        )
-        await query.message.edit_text(
-            text="▣▣▣▣▢"
-        )
-        await query.message.edit_text(
-            text="▣▣▣▣▣"
-        )
-        await message.reply_chat_action("typing")
-        dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK, dai=date, mento=day, sarch=search, tim=time), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
+        dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK, dai=date, mento=day, sarch=search, tim=time, iu=ido), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
         await asyncio.sleep(10)
         await dp.delete() 
         await message.delete(1)
