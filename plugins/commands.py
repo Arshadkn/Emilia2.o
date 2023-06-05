4d1cb476ebdf416de5dde721d2adfca0c1caf5c3
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-05 21:00:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -527,6 +527,22 @@ async def start(client, message):
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
             ]]
+        await query.message.edit_text(
+            text="▣▢▢▢▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▢▢▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▣▢▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▣▣▢"
+        )
+        await query.message.edit_text(
+            text="▣▣▣▣▣"
+        )
+        await query.message.edit_text
         dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK, dai=date, mento=day, sarch=search, tim=time), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
         await asyncio.sleep(10)
         await dp.delete() 
