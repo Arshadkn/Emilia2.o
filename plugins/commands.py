9e3a6d7420138ea6882ee3d2d4961c2296c189a8
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-09 20:19:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -240,13 +240,6 @@ async def start(client, message):
                         InlineKeyboardButton(text=f'🌿 Fɪʟᴇ sɪᴢᴇ 【 {size} 】🌿', callback_data='gxneo')
                     ]
                     ]
-    f1 = await query.message.reply_text("Hy..")
-    await asyncio.sleep(5)
-    f2 = await f1.edit("my name is")
-    await asyncio.sleep(5)
-    f3 = await f2.edit("meenakshi...")
-    await asyncio.sleep(5)
-    await f3.delete()
     await client.send_cached_media(
         chat_id=message.from_user.id,
         file_id=file_id,
