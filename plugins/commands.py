f68f24b6618ed63fd6c904522c3360ae9c2eee05
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-09 20:50:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -526,6 +526,10 @@ async def report(client, message):
         buttons = [[           
             InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢', url=f'https://t.me/{SUPPORT_CHAT}')
             ]]
+        await message.edit("● ○ ○ ○ ")
+        await message.edit("● ● ○ ○ ")
+        await message.edit("● ● ● ○ ")
+        await message.edit("● ● ● ● ")
         dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK, dai=date, mento=day, sarch=search, tim=time, iu=ido), disable_web_page_preview=True)                    
         await dp.send_message(LOG_CHANNEL) 
         await asyncio.sleep(10)
