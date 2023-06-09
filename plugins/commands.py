edc43d39b7a9171d7368bf65bf9dac277d17614b
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-09 20:28:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -525,10 +525,8 @@ async def report(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[           
             InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢', url=f'https://t.me/{SUPPORT_CHAT}')
-            ],[
-            InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
             ]]
-        dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK, dai=date, mento=day, sarch=search, tim=time, iu=ido), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)                    
+        dp = await message.reply(START_MESSAGE.format(user=message.from_user.mention if message.from_user else message.chat.title, bot=temp.B_LINK, dai=date, mento=day, sarch=search, tim=time, iu=ido), disable_web_page_preview=True)                    
         await asyncio.sleep(10)
         await dp.delete() 
         await message.delete(1)
