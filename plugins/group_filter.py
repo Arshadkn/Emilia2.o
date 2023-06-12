c53fe8d2aa48fda088f2a5aec651224c709ab005
Aditleo <124612992+Aditleo@users.noreply.github.com>
2023-06-12 23:30:16 +0530

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -290,7 +290,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b>🎪 ᴛɪᴛɪʟᴇ {search}\n\n┏ 🤴 ᴀsᴋᴇᴅ ʙʏ : {message.from_user.mention}\n┣ ⚡ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ʙʜᴀsɪ](https://t.me/+aOWzY1w5cZ44MzJl)\n┗ 🍁 ᴄʜᴀɴɴᴇʟ : [ᴄɪɴᴇᴍᴀᴄᴏᴍᴘᴀɴʏ](https://t.me/cenEma9)\n\n<i>★ ᴘᴏᴡᴇʀᴇᴅ ʙʏ  [ᴄɪɴᴇᴍᴀᴄᴏᴍᴘᴀɴʏ](https://t.me/CmRips)</i></b>"
+        cap = f"<b>🎪 ᴛɪᴛɪʟᴇ {search}\n\n┏ 🤴 ᴀsᴋᴇᴅ ʙʏ : {message.from_user.mention}\n┣ ⚡ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ᴛᴏᴠɪɴᴏ](https://t.me/+aOWzY1w5cZ44MzJl)\n┗ 🍁 ᴄʜᴀɴɴᴇʟ : [Cᴍʟɪɴᴋsᴢ](https://t.me/cenEma9)\n\n<i>★ ᴘᴏᴡᴇʀᴇᴅ ʙʏ  [ᴄɪɴᴇᴍᴀᴄᴏᴍᴘᴀɴʏ](https://t.me/CmRips)</i></b>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
